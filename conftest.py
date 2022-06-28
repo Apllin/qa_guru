75789d1ddb28bde34f065a940df9c2aa2e2018bf
saveleva_a <saveleva_a@tochka.com>
2022-06-28 19:51:49 +0100

    refactoring

--- conftest.py
+++ conftest.py
@@ -6,12 +6,13 @@ from selene.support.shared import browser
 def open_browser():
     return browser.open("https://duckduckgo.com/")
 
+
 @pytest.fixture(autouse=True)
 def quit_browser():
     yield
     browser.quit()
 
+
 @pytest.fixture(autouse=True)
 def full_size_browser(open_browser):
     browser.driver.maximize_window()
-
